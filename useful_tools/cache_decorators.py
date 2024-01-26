cbd535703cd42b02a8ed9659118560ee10c62b81
Ronny Ager-Wick <ronny@ager-wick.com>
2024-01-26 09:35:33 +0100

    Added class name to cache_status key and improved cache_status log messages

--- useful_tools/cache_decorators.py
+++ useful_tools/cache_decorators.py
@@ -98,65 +98,75 @@ print(myclass.cache_status) # gives info about the use of cache in the previous 
         # create a dictionary to store the cache status for each function
         if not hasattr(self, "cache_status"):
             self.cache_status = {}
-        self.cache_status[f"{func.__name__}_{arg_hash}"] = []
+        cache_status_key = f"{self.__class__.__name__}.{func.__name__}_{arg_hash}"
+        self.cache_status[cache_status_key] = []
 
         # If cache is disabled, call the function and return the result
         # when mocking requests, the cache is disabled
         # this is done in handle_request() in main.py
         if not self.cache_enabled:
-            self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_disabled")
-            self.cache_status[f"{func.__name__}_{arg_hash}"].append("method_called")
+            self.cache_status[cache_status_key].append("cache_disabled")
+            self.cache_status[cache_status_key].append("method_called")
             return func(self, *args, **kwargs)
         
         # Create cache directory if it doesn't exist
         if not os.path.exists(self.cache_dir):
             os.makedirs(self.cache_dir)
         
-        # Create a unique filename based on the function name and arguments
-        filename = f"{self.__class__.__name__}.{func.__name__}_{arg_hash}.pkl"
+        # Create a unique filename based on the class name, method name and arguments
+        filename = f"{cache_status_key}.pkl"
         filepath = os.path.join(self.cache_dir, filename)
 
         read_from_cache = False
         if self.ignore_cache_expiration:
             if self.force_cache_expiration:
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("ignore_cache_expiration and force_cache_expiration are both True - force_cache_expiration takes precedence")
+                self.cache_status[cache_status_key].append("ignore_cache_expiration and force_cache_expiration are both True - force_cache_expiration takes precedence")
             else:
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_expiration_ignored")
+                self.cache_status[cache_status_key].append("cache_expiration_ignored")
                 read_from_cache = True
 
         if self.force_cache_expiration:
-            self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_expiration_forced")
+            self.cache_status[cache_status_key].append("cache_expiration_forced")
             read_from_cache = False
         else:
             if self.cache_expiration is not None:
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_expiration_set")
+                self.cache_status[cache_status_key].append(f"cache_expiration_set: {self.cache_expiration}s")
                 read_from_cache = True
             else:
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_expiration_not_set")
+                self.cache_status[cache_status_key].append("cache_expiration_not_set")
                 read_from_cache = False
             
         if read_from_cache:
             if not os.path.exists(filepath):
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_file_does_not_exist")
+                self.cache_status[cache_status_key].append("cache_file_does_not_exist")
             else:
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_file_exists")
+                self.cache_status[cache_status_key].append("cache_file_exists")
                 with open(filepath, 'rb') as f:
                     cache_time, result = pickle.load(f)
+                    time_since_cache = time.time() - cache_time
                     if self.ignore_cache_expiration \
-                    or time.time() - cache_time < self.cache_expiration:
-                        self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_loaded")
+                    or time_since_cache < self.cache_expiration:
+                        self.cache_status[cache_status_key].append("cache_loaded")
                         return result
                     else:
-                        self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_expired")
+                        if time_since_cache < 10:
+                            time_since_cache_formatted = f"{time_since_cache:.3f}s"
+                        elif time_since_cache < 60:
+                            time_since_cache_formatted = f"{time_since_cache:.1f}s"
+                        elif time_since_cache < 3600:
+                            time_since_cache_formatted = f"{time_since_cache/60:.1f}m"
+                        else:
+                            time_since_cache_formatted = f"{time_since_cache/3600:.1f}h"
+                        self.cache_status[cache_status_key].append(f"cache_expired: {time_since_cache_formatted} passed")
         
         # call the function - this will happen if the cache_expiration is not set or the cache file doesn't exist or is expired
         result = func(self, *args, **kwargs)
-        self.cache_status[f"{func.__name__}_{arg_hash}"].append("method_called")
+        self.cache_status[cache_status_key].append("method_called")
 
         # If cache is enabled for the model (or cache is forced to expire), save the result to the cache
         if self.cache_expiration is not None or self.force_cache_expiration:
             with open(filepath, 'wb') as f:
                 pickle.dump((time.time(), result), f)
-                self.cache_status[f"{func.__name__}_{arg_hash}"].append("cache_saved")
+                self.cache_status[cache_status_key].append("cache_saved")
         return result
     return wrapper
