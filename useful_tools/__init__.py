05178ded6e5bb9948f57affa4ed49c861f245f20
Ronny Ager-Wick <ronny@ager-wick.com>
2024-02-04 14:24:33 +0100

    expose execute_with_cache function

--- useful_tools/__init__.py
+++ useful_tools/__init__.py
@@ -1,2 +1,2 @@
 from .act_as_list import act_as_list
-from .cache_decorators import cache_property, cache_to_memory, cache_to_disk, delete_last_saved_cache_file
+from .cache_decorators import cache_property, cache_to_memory, cache_to_disk, delete_last_saved_cache_file, execute_with_cache
