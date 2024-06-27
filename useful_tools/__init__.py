3f7e0834490e58090aa7a06b5aa17bbd0996ae1f
Ronny Ager-Wick <ronny.ager-wick@morrowbatteries.com>
2024-06-27 08:29:26 +0200

    improved exit_if_already_running and added is_process_running and kill_process

--- useful_tools/__init__.py
+++ useful_tools/__init__.py
@@ -2,7 +2,7 @@ from .act_as_list import act_as_list
 from .cache_to_memory import cache_property, cache_to_memory
 from .cache_to_disk import cache_to_disk, execute_with_cache
 from .modified_dataclasses import modified_dataclass
-from .exit_if_already_running import exit_if_already_running
+from .exit_if_already_running import exit_if_already_running, is_process_running, kill_process
 from .redirect_stdout import redirect_stdout
 from .ip_address import is_public_ip_address, is_valid_ip_address, is_reserved_ip_address
 from .git_tools import get_git_info, get_last_commit_datetime
