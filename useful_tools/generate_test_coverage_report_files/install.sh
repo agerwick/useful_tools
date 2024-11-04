96fddf17a099366c1300e5d3a5f16175a9b260cb
Ronny Ager-Wick <ronny.ager-wick@morrowbatteries.com>
2024-11-04 18:17:48 +0100

    fixed issue copying files for test coverage report

--- useful_tools/generate_test_coverage_report_files/install.sh
+++ useful_tools/generate_test_coverage_report_files/install.sh
@@ -49,11 +49,11 @@ else
     exit 1
 fi
 echo "copying .coveragerc"
-cp .venv/lib/site-packages/useful_tools/generate_test_coverage_report_files/.coveragerc .coveragerc
+cp .venv/lib/site-packages/useful_tools/generate_test_coverage_report_files/.coveragerc .
 echo "copying .githooks"
 cp -r .venv/lib/site-packages/useful_tools/generate_test_coverage_report_files/.githooks .githooks
 echo "copying run_test_coverage_report.py"
-cp .venv/lib/site-packages/useful_tools/generate_test_coverage_report_files/run_test_coverage_report.py run_test_coverage_report.py
+cp .venv/lib/site-packages/useful_tools/generate_test_coverage_report_files/run_test_coverage_report.py .
 echo "Adding .githooks folder to github hooks path..."
 git config core.hooksPath .githooks
 echo "making run_test_coverage_report.sh executable..."
